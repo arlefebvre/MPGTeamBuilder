5612882f0538080234c1fd30e5499d2b4677f54c
ALefebvre <alefebvre@isagri.fr>
2017-04-21 13:53:01 +0200

    first commit

---
+++ src/main/java/fr/arlefebvre/perso/MpgTeamBuilderApplication.java
@@ -0,0 +1,12 @@
+package fr.arlefebvre.perso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpgTeamBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpgTeamBuilderApplication.class, args);
+	}
+}
